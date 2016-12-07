1714e9cec1df415b87042ddc7c52cbf0384a3175
Andrew <andrew.george.sinclair@gmail.com>
2016-12-07 13:54:51 -0600

    Finished day-7

---
+++ day-7/src/day_7/core.clj
@@ -0,0 +1,79 @@
+(ns day-7.core
+  (:require [clojure.string :as string])
+  (:gen-class))
+
+(defn tuplize
+  [n xs]
+  (->>
+    (range n)
+    (map #(drop % xs))
+    (apply map vector)))
+
+(defn abba-reducer
+  [_ [a b b' a']]
+  (if (and
+        (= a a')
+        (= b b')
+        (not= a b))
+    (reduced true)))
+
+(defn is-abba?
+  [text]
+  (->>
+    text
+    (tuplize 4)
+    (reduce abba-reducer false)))
+
+(defn aba-reducer
+  [hypernets _ [a b a']]
+  (if (and
+        (= a a')
+        (not= a b)
+        (some #(if % (.contains % (str b a b))) hypernets))
+    (reduced true)))
+
+(defn is-aba-bab?
+  [supernet hypernets]
+  (->>
+    supernet
+    (tuplize 3)
+    (reduce (partial aba-reducer hypernets) false)))
+
+(defn parse-input
+  [input]
+  (->>
+    input
+    string/split-lines
+    (map #(string/split % #"[\[\]]"))
+    (map #(partition-all 2 %))
+    (map #(vector (map first %) (map second %)))))
+
+(defn calc-part-1
+  [data]
+  (->>
+    data
+    (filter
+      (fn [[supernets hypernets]]
+        (and
+          (some is-abba? supernets)
+          (every? (complement is-abba?) hypernets))))
+    count))
+
+(defn calc-part-2
+  [data]
+  (->>
+    data
+    (filter
+      (fn [[supernets hypernets]]
+         (some #(is-aba-bab? % hypernets) supernets)))
+    count))
+
+(defn -main
+  "Advent of Code - Day 7 - IPv7 (no it's not a real thing)"
+  [& args]
+  (let [filename "resources/input.txt"
+        input    (slurp filename)
+        data     (parse-input input)]
+    (do
+      (println (calc-part-1 data))
+      (println (calc-part-2 data)))))
