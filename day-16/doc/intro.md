df282c8c85b750e62b7b7b2a2b568b1bcb3d5d08
AndrewSinclair <andrew.george.sinclair@gmail.com>
2016-12-30 00:46:09 -0600

    Day 17 complete.  I refactored to use iterate instead of let and loop-recur

--- day-16/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to day-16
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- day-17/src/day_17/core.clj
+++ day-17/src/day_17/core.clj
@@ -3,7 +3,7 @@
   (:gen-class))
 
 (def input "qtetzkpl")
-(def empty-queue clojure.lang.PersistentQueue/EMPTY)
+(def empty-queue (clojure.lang.PersistentQueue/EMPTY))
 
 (defn next-moves
   [md5-hash]
@@ -12,33 +12,82 @@
       (take 4)
       (map #(.contains "bcdef" (str %)))))
 
-(defn update-queue
-  [queue path up down left right x y]
-  (let [queue  (if (and up    (>  y 0)) (conj queue [(str path "U")  x       (dec y)]) queue)
-        queue  (if (and down  (<= y 3)) (conj queue [(str path "D")  x       (inc y)]) queue)
-        queue  (if (and left  (>  x 0)) (conj queue [(str path "L") (dec x) y      ]) queue)
-        queue  (if (and right (<= x 3)) (conj queue [(str path "R") (inc x) y      ]) queue)]
+(defn in-maze?
+  [x y]
+  (and
+    (not (neg? x))
+    (not (neg? y))
+    (<= x 3)
+    (<= y 3)))
+
+(defn update-queue-dir
+  [path direction symbol x y queue]
+  (if (and (in-maze? x y) direction)
+    (conj queue {:path  (str path symbol)
+                 :x     x
+                 :y     y})
     queue))
 
+(defn update-queue
+  [queue path x y [up down left right]]
+    (if (= x y 3)
+      queue
+      (->>
+        queue
+        (update-queue-dir path up    "U"      x (dec y))
+        (update-queue-dir path down  "D"      x (inc y))
+        (update-queue-dir path left  "L" (dec x)     y)
+        (update-queue-dir path right "R" (inc x)     y))))
+
+(defn bfs-iterator
+  [[{path :path x :x y :y} & rest-queue]]
+    (->>
+      path
+      (str input)
+      digest/md5
+      next-moves
+      (update-queue (apply conj empty-queue rest-queue) path x y)))
+
+(defn iterator-end-condition
+  [[{_ :path x :x y :y} & _]]
+  (= x y 3))
+
 (defn calc-part-1
-  [input]
-  (loop [queue (conj empty-queue ["" 0 0])]
-    (let [[path x y]            (first queue)
-          md5-hash              (digest/md5 (str input path))
-          [up down left right]  (next-moves md5-hash)
-          queue                 (update-queue (pop queue) path up down left right x y)]
-      (cond
-        (empty? queue)        "There is no exit"
-        (and (= x 3) (= y 3)) path
-        :else                 (recur queue)))))
+  "Find shortest path. I use a breadth-first-search (bfs) so it always
+   expands the search state tree by the shortest nodes first, hence when
+   it finds a path that reaches the vault, all paths shorter than it 
+   have been checked already and confirmed to not reach the vault."
+   []
+  (->>
+    {:path "" :x 0 :y 0}
+    (conj empty-queue)
+    (iterate bfs-iterator)
+    (filter iterator-end-condition)
+    first
+    first
+    :path))
 
 (defn calc-part-2
-  [input]
-  nil)
+  "Find longest path (and return the length). I use the same BFS as in part 1,
+  but I don't stop at the first path this time. Instead I generate all paths
+  (for which BFS is neither better nor worse than other strategies), then filter
+  on which finish at the vault, then sort the path lengths.
+  Note, generating all paths in the BFS means iterating until the queue is empty,
+  so long as no illegal paths are ever entered into the queue."
+  []
+  (->>
+    {:path "" :x 0 :y 0}
+    (conj empty-queue)
+    (iterate bfs-iterator)
+    (take-while (comp not empty?))
+    (filter (fn [[{x :x y :y}]] (= x y 3)))
+    (map #(-> % first :path count))
+    (sort >)
+    first))
 
 (defn -main
   "Advent of Code - Day 17 - Vault Maze with MD5"
   [& args]
   (do
-    (println (calc-part-1 input))
-    (println (calc-part-2 input))))
+    (println (calc-part-1))
+    (println (calc-part-2))))
